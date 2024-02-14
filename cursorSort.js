7c62a84b2b9edb38b3710be3939737d67761fb18
yathisha6807 <78218761+yathisha6807@users.noreply.github.com>
2024-02-13 22:07:05 -0500

    cursorSort.js

---
+++ cursorSort.js
@@ -0,0 +1,14 @@
+//ascending order
+ db.listingsAndReviews.find({bed_type: "Real Bed"}).sort({name: 1});
+ //descending order
+ db.listingsAndReviews.find({bed_type: "Real Bed"}).sort({name: -1});
+
+ //to simplify for understanding, we can use projections
+ db.listingsAndReviews.find({bed_type: "Real Bed"}, {name: 1}).sort({name: 1});
+ db.listingsAndReviews.find({bed_type: "Real Bed"}, {name: -1}).sort({name: -1});
+
+ //limiting results to n
+ db.listingsAndReviews
+   .find({bed_type: "Real Bed"}, {property_type: "Apartment"})
+   .sort({name: -1})
+   .limit(3);
