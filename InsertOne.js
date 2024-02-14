6dbf20eeed87bb37fec4f233086d86f1cf789f17
yathisha6807 <78218761+yathisha6807@users.noreply.github.com>
2024-02-13 21:51:21 -0500

    InsertOne.js

---
+++ InsertOne.js
@@ -0,0 +1,7 @@
+db.books.insertOne({
+   title: "Deep Dive into React Hooks",
+   ISBN: "000000000",
+   thumbnailUrl: "",
+   publicationDate: ISODate("2019-01-01T00:00:00.000Z"),
+   authors: ["Ada Lovelace"],
+ });
