6da21edbfc3e425168f5663a25396a5e53188c8c
yathisha6807 <78218761+yathisha6807@users.noreply.github.com>
2024-02-21 13:26:12 -0500

    insertioneUsingCrud.py

---
+++ insertioneUsingCrud.py
@@ -0,0 +1,41 @@
+from pymongo import MongoClient
+from pymongo.server_api import ServerApi
+import datetime
+from pprint import pprint
+
+# Define the MongoDB connection URI
+uri = "mongodb+srv://yathishaag:Vinnie$27082001@cluster0.2z6l7np.mongodb.net/?retryWrites=true&w=majority"
+
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+try:
+    # Send a ping to confirm a successful connection
+    client.admin.command('ping')
+
+    # Get reference to 'music_platform' database
+    db = client.music_platform
+
+    # Get reference to 'songs' collection
+    songs_collection = db.songs
+
+    # Inserting one song
+    new_song = {
+        "title": "Flowers”,
+        "artist": “Miley “Cyrus,
+        "genre": "Rock",
+        "duration_seconds": 354,
+        "created_at": datetime.datetime.utcnow()
+    }
+
+    # Write an expression that inserts the 'new_song' document into the 'songs' collection.
+    result = songs_collection.insert_one(new_song)
+
+    song_id = result.inserted_id
+    pprint(f"_id of inserted song: {song_id}”)
+
+except Exception as e:
+    print(e)
+finally:
+    client.close()
+
