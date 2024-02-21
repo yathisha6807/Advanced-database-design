c19c91e61fd06c0d37369613359c4964e7b9811c
yathisha6807 <78218761+yathisha6807@users.noreply.github.com>
2024-02-21 13:34:52 -0500

    deletemanyusingCrud.py

---
+++ deletemanyusingCrud.py
@@ -0,0 +1,33 @@
+from pymongo import MongoClient
+from pymongo.server_api import ServerApi
+from pprint import pprint
+
+# Define the MongoDB connection URI
+uri = "mongodb+srv://yathishaag:Vinnie$27082001@cluster0.2z6l7np.mongodb.net/?retryWrites=true&w=majority"
+
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+try:
+    # Send a ping to confirm a successful connection
+    client.admin.command('ping')
+
+    # Get reference to 'music_platform' database
+    db = client.music_platform
+
+    # Get reference to 'songs' collection
+    songs_collection = db.songs
+
+    # Define the criteria for deleting all documents (an empty filter matches all documents)
+    query = {}
+
+    # Delete all documents from the 'songs' collection
+    result = songs_collection.delete_many(query)
+
+    # Print information about the deletion result
+    pprint(f"Deleted {result.deleted_count} document(s).")
+
+except Exception as e:
+    print(e)
+finally:
+    client.close()
